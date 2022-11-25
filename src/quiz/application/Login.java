67f44e5e8a62836b048ceafaccdd2874f360d5fd
AjeetSingh <ajeetsingh8139@gmail.com>
2022-11-25 21:57:13 +0530

    First Commit

---
+++ src/quiz/application/Login.java
@@ -0,0 +1,70 @@
+package quiz.application;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Login extends JFrame implements ActionListener{
+ 
+    JButton rules, back;
+    JTextField tfname;
+    
+    Login() {
+        getContentPane().setBackground(Color.white);
+        setLayout(null);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/login.png"));
+        JLabel image = new JLabel(i1);
+        image.setBounds(-130, -230, 800, 900);
+        add(image);
+        
+        JLabel heading = new JLabel("Simple Minds");
+        heading.setBounds(550, 60, 300, 45);
+        heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 40));
+        heading.setForeground(new Color(30, 144, 254));
+        add(heading);
+        
+        JLabel name = new JLabel("Enter your name");
+        name.setBounds(610, 150, 300, 20);
+        name.setFont(new Font("Mongolian Baiti", Font.BOLD, 18));
+        name.setForeground(new Color(30, 144, 254));
+        add(name);
+        
+        tfname = new JTextField();
+        tfname.setBounds(535, 200, 300, 25);
+        tfname.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        add(tfname);
+        
+        rules = new JButton("Rules");
+        rules.setBounds(535, 270, 120, 25);
+        rules.setBackground(new Color(30, 144, 254));
+        rules.setForeground(Color.WHITE);
+        rules.addActionListener(this);
+        add(rules);
+        
+        back = new JButton("Back");
+        back.setBounds(715, 270, 120, 25);
+        back.setBackground(new Color(30, 144, 254));
+        back.setForeground(Color.WHITE);
+        back.addActionListener(this);
+        add(back);
+        
+        setSize(900, 500);
+        setLocation(270, 180);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == rules) {
+            String name = tfname.getText();
+            setVisible(false);
+            new Rules(name);
+        } else if (ae.getSource() == back) {
+            setVisible(false);
+        }
+    }
+    
+    public static void main(String[] args) {
+        new Login();
+    }
+}
---
+++ src/quiz/application/Rules.java
@@ -0,0 +1,71 @@
+package quiz.application;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Rules extends JFrame implements ActionListener{
+
+    String name;
+    JButton start, back;
+    
+    Rules(String name) {
+        this.name = name;
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel heading = new JLabel("Welcome " + name + " to Simple Minds");
+        heading.setBounds(50, 20, 700, 30);
+        heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 28));
+        heading.setForeground(new Color(30, 144, 254));
+        add(heading);
+        
+        JLabel rules = new JLabel();
+        rules.setBounds(20, 90, 700, 350);
+        rules.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        rules.setText(
+            "<html>"+ 
+                "1. Read each question carefully, and Mark the correct answer and click on the Next button to go to the next question." + "<br><br>" +
+                "2. You can only take 50-50 chances once." + "<br><br>" +
+                "3. You will get 15 seconds for each question." + "<br><br>" +
+                "4. The total score for the quiz is based on your responses to all questions." + "<br><br>" +
+                "5. After responding to the 10th question, click on \"Submit\" on the top of the window to exit the quiz." + "<br><br>" +
+                "6. If there is any tie up among the participants about average score, it will be solved by taking another round or set of rounds." + "<br><br>" +
+            "<html>"
+        );
+        add(rules);
+        
+        back = new JButton("Back");
+        back.setBounds(250, 500, 100, 30);
+        back.setBackground(new Color(30, 144, 254));
+        back.setForeground(Color.WHITE);
+        back.addActionListener(this);
+        add(back);
+        
+        start = new JButton("Start");
+        start.setBounds(400, 500, 100, 30);
+        start.setBackground(new Color(30, 144, 254));
+        start.setForeground(Color.WHITE);
+        start.addActionListener(this);
+        add(start);
+        
+        setSize(800, 650);
+        setLocation(350, 100);
+        setVisible(true);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == start) {
+            setVisible(false);
+            new Quiz(name);
+        } else {
+            setVisible(false);
+            new Login();
+        }
+    }
+    
+    public static void main(String[] args) {
+        new Rules("User");
+    }
+}
